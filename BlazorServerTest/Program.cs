332f7e92b21cf513a40c04a5b1d0af45e61bbfe9
Dmytro Riabchenko <ryabchenko2255@gmail.com>
2024-06-11 17:21:59 +0300

    Added MudBlazor to project and added component for test

--- BlazorServerTest/Program.cs
+++ BlazorServerTest/Program.cs
@@ -2,6 +2,7 @@ using BlazorServerTest.Data;
 using BlazorServerTest.Data.Sources;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<SourceService>();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
